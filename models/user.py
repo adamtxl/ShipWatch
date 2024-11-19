2d38b89c0afc2cd0d69552a3c61072fd1105a4ad
Adam Troxell <adamtxl@hotmail.com>
2024-11-19 13:13:17 -0600

    Setting up basic CRUD for users, reset the database file since this will be done through models

---
+++ models/user.py
@@ -0,0 +1,10 @@
+from models import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)  # Hash later for security
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
--- routes/cruises.py
+++ routes/cruises.py
@@ -1,22 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 # Define the Blueprint
 cruises_bp = Blueprint('cruises', __name__)
 
-# Example route for searching cruises
-@cruises_bp.route('/search', methods=['GET'])
-def search_cruises():
-    # Mock response for now
-    return {
-        "message": "Cruise search endpoint working!",
-        "data": [
-            {"destination": "Bahamas", "price": "$500"},
-            {"destination": "Alaska", "price": "$1200"},
-            {"destination": "Mediterranean", "price": "$900"}
-        ]
-    }
+# Mock cruise data
+mock_cruises = [
+    {"name": "Caribbean Escape", "price": "$799", "link": "http://example.com/book-caribbean"},
+    {"name": "Alaska Adventure", "price": "$1200", "link": "http://example.com/book-alaska"},
+    {"name": "Mediterranean Getaway", "price": "$999", "link": "http://example.com/book-med"}
+]
 
-# Example route for tracking cruises
-@cruises_bp.route('/track', methods=['POST'])
-def track_cruise():
-    return {"message": "Track cruise endpoint working!"}
+# Route for searching cruises
+@cruises_bp.route('/search', methods=['POST'])
+def search_cruises():
+    # Get search criteria from the request
+    data = request.get_json()
+    destination = data.get('destination', '').lower() if data else ''
+
+    # Filter mock data based on destination
+    filtered_cruises = [cruise for cruise in mock_cruises if destination in cruise['name'].lower()]
+
+    # Return filtered results or all cruises if no filter is applied
+    return jsonify({"cruises": filtered_cruises or mock_cruises})
